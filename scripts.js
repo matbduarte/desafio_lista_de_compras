7cf9b34407900ed0cd0e98764794765c6fab3962
Mateus Duarte <matbduarte@gmail.com>
2025-11-14 18:21:57 -0300

    Finished creating everything

--- scripts.js
+++ scripts.js
@@ -1,16 +1,92 @@
+const backBtn = document.querySelector('#back-btn')
 let newitemInput = document.querySelector("#newitem")
 const button = document.querySelector("button")
 const itemsList = document.querySelector("ul")
+const deleteMessage = document.querySelector(".delete-message")
+const closeDeleteMsgBtn = document.querySelector("#close-delete-msg")
 
-button.addEventListener("click", (event) => {
-    event.preventDefault()
-    console.log("CLICOU NO BOTÃO")
-    console.log("newitemInput:", newitemInput.value)
-    addItemToList(newitemInput.value)
-    newitemInput.value = null
+// Set up event listeners for all existing items on page load
+document.addEventListener("DOMContentLoaded", () => {
+    const existingItems = document.querySelectorAll(".item.checkbox-wrapper")
+    for (const item of existingItems) {
+        setupListItemEventListeners(item)
+    }
 })
 
-function addItemToList (itemName) {
+// Update garbage icon based on checkbox state
+function updateGarbageIcon(item) {
+    const checkboxInput = item.querySelector("input[type='checkbox']")
+    const garbageImg = item.querySelector("img[src='assets/garbage.svg'], img[src='assets/garbage-selected.svg']")
+    
+    if (checkboxInput && garbageImg) {
+        if (checkboxInput.checked) {
+            garbageImg.src = "assets/garbage-selected.svg"
+            garbageImg.style.cursor = "pointer"
+        } else {
+            garbageImg.src = "assets/garbage.svg"
+            garbageImg.style.cursor = ""
+        }
+    }
+}
+
+// Set up event listeners for a single list item
+function setupListItemEventListeners(item) {
+    const checkboxImg = item.querySelector(".checkbox-image")
+    const checkboxInput = item.querySelector("input[type='checkbox']")
+    const garbageImg = item.querySelector("img[src='assets/garbage.svg'], img[src='assets/garbage-selected.svg']")
+    
+    // Update garbage icon based on initial checkbox state
+    updateGarbageIcon(item)
+    
+    if (checkboxImg && checkboxInput) {
+        checkboxImg.addEventListener("click", (event) => {
+            event.stopPropagation()
+            checkboxInput.checked = !checkboxInput.checked
+            updateGarbageIcon(item)
+        })
+    }
+    
+    // Also listen to checkbox input changes (in case it's clicked directly)
+    if (checkboxInput) {
+        checkboxInput.addEventListener("change", () => {
+            updateGarbageIcon(item)
+        })
+    }
+    
+    if (garbageImg) {
+        garbageImg.addEventListener("click", (event) => {
+            event.stopPropagation()
+            // Only show delete message if the garbage icon is selected (garbage-selected.svg)
+            if (garbageImg.src.includes("garbage-selected.svg")) {
+                showDeletedMessage();
+                // Remove the item from the list
+                item.remove()
+            }
+        })
+    }
+}
+
+// restart page with empty list and empty input field
+backBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    newitemInput.value = null
+    itemsList.innerHTML = ''
+})
+
+// if we click on "Adicionar item" adds the new item
+button.addEventListener("click", (event) => {
+    event.preventDefault()
+    // validation if newItem field is empty
+    const charRegex = /[A-Za-z]/g
+    if (charRegex.test(newitemInput.value)) {
+        const newItem = addItemToList(newitemInput.value)
+        setupListItemEventListeners(newItem)
+        newitemInput.value = null
+    }
+})
+
+// add newItem to itemsList
+function addItemToList(itemName) {
     // create li class="item checkbox-wrapper"
     const newItem = document.createElement("li")
     newItem.classList.add("item", "checkbox-wrapper")
@@ -32,10 +108,22 @@ function addItemToList (itemName) {
     newItem.append(garbageImg)
     // add new item into the list
     itemsList.append(newItem)
+    // return the new item so event listeners can be set up
+    return newItem
+}
+
+// close delete message warning
+closeDeleteMsgBtn.addEventListener("click", () => {
+    deleteMessage.classList.remove("unhide")
+})
+
+// show delete message warning
+function showDeletedMessage() {
+    deleteMessage.classList.add("unhide")
 }
 
 // TODO:
-// - adicionar validação para caso o campo de input esteja vazio
-// - ao clicar em voltar, esvaziar a lista
-// - somente mostrar a div delete-message ao clicar na lixeira de algum item (display: flex)
-// - esconder a div delete-message ao clicar no X dela (display: none)
+// - (OK) adicionar validação para caso o campo de input esteja vazio
+// - (OK) esconder a div delete-message ao clicar no X dela (display: none)
+// - (OK) somente mostrar a div delete-message ao clicar na lixeira de algum item (display: flex)
+// - (OK) ao clicar em voltar, esvaziar a lista
